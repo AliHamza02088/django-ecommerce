bc36f6a3e83d4f6f58dfefbf7d9f886adf9bdfe0
Alihamza <ch2953917@gmail.com>
2024-12-11 16:36:58 +0500

    added the templates, urls, views and static folder and files and corrected the index.html

--- store/urls.py
+++ store/urls.py
@@ -1,1 +1,23 @@
-print("Hello World")
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index_view, name="index"),
+    path("about_us/", views.about_us_view, name="about_us"),
+    path("checkout_complete/", views.checkout_complete_view, name="checkout_complete"),
+    path("checkout_info/", views.checkout_info_view, name="checkout_info"),
+    path("checkout_payment/", views.checkout_payment_view, name="checkout_payment"),
+    path("contact_us/", views.contact_us_view, name="contact_us"),
+    path("faq/", views.faq_view, name="faq"),
+    path("index_fixed/", views.index_fixed_view, name="index_fixed"),
+    path(
+        "index_inverse_header/",
+        views.index_inverse_header_view,
+        name="index_inverse_header",
+    ),
+    path("my_account/", views.my_account_view, name="my_account"),
+    path("product/<int:product_id>/", views.product_detail_view, name="product_detail"),
+    path("products/", views.product_view, name="products"),
+    path("search_results/", views.search_results_view, name="search_results"),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_view(request):
+    return render(request, "store/index.html")
+
+
+def checkout_complete_view(request):
+    return render(request, "store/checkout_complete.html")
+
+
+def checkout_info_view(request):
+    return render(request, "store/checkout_info.html")
+
+
+def checkout_payment_view(request):
+    return render(request, "store/checkout_payment.html")
+
+
+def contact_us_view(request):
+    return render(request, "store/contact_us_view")
+
+
+def faq_view(request):
+    return render(request, "store/faq.html")
+
+
+def index_fixed_view(request):
+    return render(request, "store/index_fixed.html")
+
+
+def index_inverse_header_view(request):
+    return render(request, "store/index_inverse_header.html")
+
+
+def my_account_view(request):
+    return render(request, "store/my_account.html")
+
+
+def product_detail_view(request):
+    return render(request, "store/product_detail.html")
+
+
+def product_view(request):
+    return render(request, "store/product.html")
+
+
+def search_results_view(request):
+    return render(request, "store/search_results.html")
+
+
+def about_us_view(request):
+    return render(request, "store/about_us.html")
